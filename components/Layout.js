e6e1726a2c0d78e8c241a7ec4a0d72b2e98c6a74
Javkhlan <javhlan0420@gmail.com>
2022-08-29 14:22:23 +0800

    meta

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,10 @@
 import Head from 'next/head';
 import Footer from './Footer';
 import Navbar from './Navbar';
+import { useRouter } from 'next/router';
 
 export default function Layout({ children }) {
+	const router = useRouter();
 	const meta = {
 		title: 'Jak - Developer',
 		description: `Software Engineer based in Mongolia, a student at National University of Mongolia.`,
@@ -16,15 +18,21 @@ export default function Layout({ children }) {
 				<title>{meta.title}</title>
 				<meta name='robots' content='follow, index' />
 				<meta content={meta.description} name='description' />
-				{/* <meta property='og:url' content={`https://leerob.io${router.asPath}`} />
-        <link rel='canonical' href={`https://leerob.io${router.asPath}`} /> */}
+				<meta
+					property='og:url'
+					content={`https://jak-dev.vercel.app${router.asPath}`}
+				/>
+				<link
+					rel='canonical'
+					href={`https://jak-dev.vercel.app${router.asPath}`}
+				/>
 				<meta property='og:type' content={meta.type} />
 				<meta property='og:site_name' content='Jak' />
 				<meta property='og:description' content={meta.description} />
 				<meta property='og:title' content={meta.title} />
 				{/* <meta property='og:image' content={meta.image} /> */}
 				<meta name='twitter:card' content='summary_large_image' />
-				{/* <meta property='twitter:url' content='https://abdulrahman.id/' /> */}
+				<meta property='twitter:url' content='https://jak-dev.vercel.app' />
 				<meta name='twitter:title' content={meta.title} />
 				<meta name='twitter:description' content={meta.description} />
 				{/* <meta name='twitter:image' content={meta.image} /> */}
